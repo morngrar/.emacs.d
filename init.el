fc870fcb32eb2cd07280bc41c81de0171363e4d3
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-08-30 13:11:25 +0200

    Boilerplate and basic org-mode added.

---
+++ init.el
@@ -0,0 +1,33 @@
+
+;; Boilerplate
+
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode)
+  )
+
+
+;; Org-mode
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook
+	    (lambda () (org-bullets-mode 1))))
+
