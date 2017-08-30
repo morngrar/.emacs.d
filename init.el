bbf1c43efe8208a231bc812f0e596b634926c72e
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-08-30 17:19:08 +0200

    Added auto-completion, and cleaned swiper config a bit.

--- init.el
+++ init.el
@@ -46,30 +46,41 @@
   )
 (use-package swiper
   :ensure t
+  :bind
+  (progn
+    ("C-x C-f" . counsel-find-file)
+    ("C-s" . swiper)
+    )
   :config
   (progn
     (ivy-mode 1)
     (setq ivy-use-virtual-buffers t)
     (setq enable-recursive-minibuffers t)
-    (global-set-key "\C-s" 'swiper)
-    (global-set-key (kbd "C-c C-r") 'ivy-resume)
-    (global-set-key (kbd "<f6>") 'ivy-resume)
-    (global-set-key (kbd "M-x") 'counsel-M-x)
-    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-    (global-set-key (kbd "<f1> l") 'counsel-find-library)
-    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    (global-set-key (kbd "C-c g") 'counsel-git)
-    (global-set-key (kbd "C-c j") 'counsel-git-grep)
-    (global-set-key (kbd "C-c k") 'counsel-ag)
-    (global-set-key (kbd "C-x l") 'counsel-locate)
-    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ;(global-set-key "\C-s" 'swiper)
+    ;(global-set-key (kbd "C-c C-r") 'ivy-resume)
+    ;(global-set-key (kbd "<f6>") 'ivy-resume)
+    ;(global-set-key (kbd "M-x") 'counsel-M-x)
+    ;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    ;(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    ;(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    ;(global-set-key (kbd "<f1> l") 'counsel-find-library)
+    ;(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    ;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    ;(global-set-key (kbd "C-c g") 'counsel-git)
+    ;(global-set-key (kbd "C-c j") 'counsel-git-grep)
+    ;(global-set-key (kbd "C-c k") 'counsel-ag)
+    ;(global-set-key (kbd "C-x l") 'counsel-locate)
+    ;(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
     ))
 
-
+;; GLOBAL Auto-completion
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
 
 
 ;; Org-mode
