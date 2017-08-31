720f62302a734d6d06f8b206ce2a7ec97d30a553
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-08-31 12:02:02 +0200

    Fixed a lisp error in init file.

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 
 ;; GLOBAL Boilerplate
-
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 
@@ -46,21 +45,17 @@
   )
 (use-package swiper
   :ensure t
-  :bind
-  (progn
-    ("C-x C-f" . counsel-find-file)
-    ("C-s" . swiper)
-    )
+  :bind (("C-x C-f" . counsel-find-file)
+	 ("C-s" . swiper)
+	 ("C-c C-r" . ivy-resume)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file)
+	 )
   :config
   (progn
     (ivy-mode 1)
     (setq ivy-use-virtual-buffers t)
     (setq enable-recursive-minibuffers t)
-    ;(global-set-key "\C-s" 'swiper)
-    ;(global-set-key (kbd "C-c C-r") 'ivy-resume)
-    ;(global-set-key (kbd "<f6>") 'ivy-resume)
-    ;(global-set-key (kbd "M-x") 'counsel-M-x)
-    ;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
     ;(global-set-key (kbd "<f1> f") 'counsel-describe-function)
     ;(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
     ;(global-set-key (kbd "<f1> l") 'counsel-find-library)
