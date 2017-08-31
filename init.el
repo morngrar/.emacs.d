f0384ed68ef93461305d4638ea9b2069518b7713
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2017-08-31 12:37:56 +0200

    Preliminary conversion to using orgfile for configuration.

--- init.el
+++ init.el
@@ -1,99 +1,18 @@
 
-;; GLOBAL Boilerplate
-(setq inhibit-startup-message t)
-(tool-bar-mode -1)
+;; This file contains boiler plate code to enable the org-file to take
+;; over.
 
+; Enable package management from MELPA
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-
 (package-initialize)
 
+; Setup "use-package"
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package try
-  :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode)
-  )
-
-;; GLOBAL  Use ibuffer instead of regular bufferlist
-(defalias 'list-buffers 'ibuffer)
-
-
-;; GLOBAL Better window management
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-   (global-set-key [remap other-window] 'ace-window)
-   (custom-set-faces
-    '(aw-leading-char-face
-      ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-   ))
-
-
-;; GLOBAL Better searching and file finding
-(use-package counsel
-  :ensure t
-  )
-(use-package swiper
-  :ensure t
-  :bind (("C-x C-f" . counsel-find-file)
-	 ("C-s" . swiper)
-	 ("C-c C-r" . ivy-resume)
-	 ("M-x" . counsel-M-x)
-	 ("C-x C-f" . counsel-find-file)
-	 )
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq enable-recursive-minibuffers t)
-    ;(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-    ;(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-    ;(global-set-key (kbd "<f1> l") 'counsel-find-library)
-    ;(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-    ;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    ;(global-set-key (kbd "C-c g") 'counsel-git)
-    ;(global-set-key (kbd "C-c j") 'counsel-git-grep)
-    ;(global-set-key (kbd "C-c k") 'counsel-ag)
-    ;(global-set-key (kbd "C-x l") 'counsel-locate)
-    ;(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
-
-;; GLOBAL Auto-completion
-(use-package auto-complete
-  :ensure t
-  :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-    ))
-
-;; Prettyfying things
-;(use-package zenburn-theme
-;  :ensure t
-;  :config
-;  (load-theme 'zenburn t)
-;  )
-
-(use-package color-theme
-  :ensure t
-  :config
-  (load-theme 'tango)
-  )
-
-;; Org-mode
-(use-package org-bullets
-  :ensure t
-  :config
-  (add-hook 'org-mode-hook
-	    (lambda () (org-bullets-mode 1))))
-
+; Load org-mode emacs-config
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-config.org"))
