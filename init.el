20e3a87293a07eeaf0994d85780eddd0905cbd46
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2018-02-06 17:31:30 +0100

    Fixed a few issues with the config on emacs 25.
    Now one has to manually installing the 'tango-plus' theme from 'M-x list-packages'

--- init.el
+++ init.el
@@ -16,3 +16,17 @@
 
 ; Load org-mode emacs-config
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-bullets which-key use-package try tango-plus-theme jedi flycheck counsel color-theme ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
