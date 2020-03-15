00d8d8cebcbc59f621a2455d52ef7f67c4feb816
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2020-03-15 15:00:20 +0100

    ledger-mode added and configured

--- init.el
+++ init.el
@@ -21,6 +21,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ledger-reports
+   (quote
+    (("prices" "ledger ")
+     ("bal" "%(binary) -f %(ledger-file) bal")
+     ("reg" "%(binary) -f %(ledger-file) reg")
+     ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+     ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
  '(package-selected-packages
    (quote
     (go-mode php-mode rust-mode company-irony company-anaconda company org-bullets which-key use-package try tango-plus-theme flycheck counsel color-theme ace-window))))
